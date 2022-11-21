39e12915ff733b80df88f84d4f6af698cdf7d93c
Mohit <mohitkhatri4272@gmail.com>
2022-11-21 11:26:42 +0530

    Doughnut chart and label structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,20 @@
 
 import './App.css';
+import Graph from './components/Graph';
 
 function App() {
   return (
     <div className="App">
-      <h1>Expense Tracker</h1>
+      <div className="container mx-auto max-w-6xl text-center drop-shadow-lg text-gray-800">
+      <h1 className="text-4xl py-8 mb-10 bg-slate-800 text-white rounded">Expense Tracker</h1>
+      
+      {/* grid columns */}
+      <div className="grid md:grid-cols-2 gap-4">
+          {/*Chart*/}
+          < Graph />
+          {/*Form*/}
+      </div>
+      </div>
     </div>
   );
 }
