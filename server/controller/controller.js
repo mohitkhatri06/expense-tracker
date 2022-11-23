4c7182675d8d721984753522211fb3618e7e55b2
Mohit <mohitkhatri4272@gmail.com>
2022-11-23 10:41:04 +0530

    connected to the mongodb

---
+++ server/controller/controller.js
@@ -0,0 +1,9 @@
+// get categories
+
+function create_Categories(req, res){
+    res.json("Get Request from Categories")
+}
+
+module.exports = {
+    create_Categories
+}
--- server/server.js
+++ server/server.js
@@ -2,15 +2,33 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 
-const port = 5000;
+require('dotenv').config({path:'./config.env'});
+const port = process.env.PORT||5000;
 
 //use middleware
 app.use(cors());
 app.use(express.json());
 
+// mongodb connection
+const con = require('./db/connection')
+
 //using routes
 app.use(require('./routes/route'))
 
-app.listen(port, () => {
-    console.log(`Server is running on port: ${port}`)
+
+con.then(db => {
+    if(!db)
+        return process.exit(1);
+
+    //listen to the http server
+    app.listen(port, () => {
+        console.log(`Server is running on port: http://localhost:${port}`)
+    })
+
+    app.on('Error', err => console.log(`Failed To Connect with HTTP Server:${err}`));
+
+    //error in mongodb connection
+}).catch(error => {
+    console.log(`Connection Failed...! ${error}`);
 })
+
